d5588c66a2cff639be98d8ed677994dcfe7424c1
dmitry selivanov <dmitryselivanov89@yandex.ru>
2024-11-30 20:46:01 +0400

    refactor(routing): add guard to routes

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -1,10 +1,30 @@
 import { Routes } from '@angular/router';
-import { AllHabitsPageComponent } from './pages/all-habits-page/all-habits-page.component';
 import { HabitFormPageComponent } from './pages/habit-form-page/habit-form-page.component';
+import { authGuard } from './guards/auth-guard';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AllHabitsPageComponent } from './pages/all-habits-page/all-habits-page.component';
+import { PageNotFoundPageComponent } from './pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: 'edit-habit/:id', component: HabitFormPageComponent },
-  { path: 'new-habit', component: HabitFormPageComponent },
-  { path: '', component: AllHabitsPageComponent, pathMatch: 'full' },
-  { path: '**', component: AllHabitsPageComponent },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: 'habits',
+    canActivate: [authGuard],
+    component: AllHabitsPageComponent,
+  },
+  {
+    path: 'edit-habit/:id',
+    canActivate: [authGuard],
+    component: HabitFormPageComponent,
+  },
+  {
+    path: 'new-habit',
+    canActivate: [authGuard],
+    component: HabitFormPageComponent,
+  },
+  { path: '', redirectTo: 'habits', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundPageComponent },
 ];
