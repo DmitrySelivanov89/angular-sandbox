0fba006976eca22505583e33afa0e7875b532105
dmitry selivanov <dmitryselivanov89@yandex.ru>
2024-11-30 20:48:47 +0400

    refactor(toolbar): add logout

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,12 +1,15 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
   OnInit,
   signal,
 } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { DatePipe } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-toolbar',
@@ -14,12 +17,20 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['toolbar.component.css'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatToolbarModule, MatIconModule, DatePipe],
+  imports: [MatToolbarModule, MatIconModule, DatePipe, MatButton],
 })
 export class ToolbarComponent implements OnInit {
+  private readonly authService = inject(AuthService);
+
+  readonly isLoggedIn = this.authService.isAuthenticated;
+
   readonly timeNow = signal(new Date());
 
   ngOnInit(): void {
     setInterval(() => this.timeNow.set(new Date()), 1000);
+  }
+
+  logout(): void {
+    this.authService.logout();
   }
 }
