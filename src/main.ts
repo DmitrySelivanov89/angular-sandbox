8b43073bb3fce5dfa60c5aee2f060d3ea3a90cf0
dmitry selivanov <dmitryselivanov89@yandex.ru>
2024-11-30 20:50:21 +0400

    rix(app-config): add config to bootstrapApplication

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { routes } from './app/app-routes';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideAnimations()],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err),
+);
