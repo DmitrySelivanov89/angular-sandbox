2962e85eb5aaac6915141a20a5efb66fcd8573a6
dmitry selivanov <dmitryselivanov89@yandex.ru>
2024-11-30 20:48:22 +0400

    feat(login-page): add login form

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,53 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: 'login-page.component.html',
+  styleUrls: ['login-page.component.css'],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatIconButton,
+    MatIcon,
+    MatLabel,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginPageComponent {
+  readonly hide = signal(true);
+
+  private readonly authService = inject(AuthService);
+  protected readonly loginForm: FormGroup = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl<string>('', [Validators.required]),
+  });
+
+  clickEvent(event: MouseEvent) {
+    event.stopPropagation();
+    this.hide.set(!this.hide());
+  }
+
+  login(event: unknown): void {
+    console.log(event);
+    this.authService.login();
+  }
+}
