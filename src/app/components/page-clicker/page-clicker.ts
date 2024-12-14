5598eb81ce167f81bc4d53ccf16078adaf9f1261
dmitry selivanov <dmitryselivanov89@yandex.ru>
2024-12-14 19:40:03 +0400

    feat(page-clicker): observable state and reset example

---
+++ src/app/components/page-clicker/page-clicker.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { fromEvent, map, merge, scan, Subject } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
+const accumulationHandler =
+  (event: PointerEvent) => (state: PointerEvent[]) => [...state, event];
+
+const resetHandler = (event: void) => (state: PointerEvent[]) => [];
+
+@Component({
+  selector: 'app-page-click-counter',
+  standalone: true,
+  imports: [AsyncPipe, MatButtonModule],
+  template: ` <section class="app-description">
+      <div>
+        <h1>Page Click Visualizer</h1>
+        <p>Start clicking around the page...</p>
+      </div>
+    </section>
+    <div>
+      <button
+        mat-stroked-button
+        (click)="reset$.next(); $event.stopPropagation()"
+      >
+        Reset State
+      </button>
+    </div>
+    @for (click of clicks$ | async; track $index) {
+      <div
+        [style.left.px]="click.clientX"
+        [style.top.px]="click.clientY"
+        class="click"
+      ></div>
+    }`,
+})
+export class PageClickCounterComponent {
+  readonly reset$ = new Subject<void>();
+
+  readonly clicks$ = merge(
+    fromEvent<PointerEvent>(document, 'click').pipe(map(accumulationHandler)),
+    this.reset$.pipe(map(resetHandler)),
+  ).pipe(
+    scan((state: PointerEvent[], stateHandlerFn) => stateHandlerFn(state), []),
+  );
+}
