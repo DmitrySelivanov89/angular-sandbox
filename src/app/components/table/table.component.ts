f954105d516320758bb6818613a4903101afd514
dmitry selivanov <dmitryselivanov89@yandex.ru>
2024-12-14 19:29:13 +0400

    feat(components): add custom table

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,42 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  contentChild,
+  effect,
+  input,
+  Signal,
+  TemplateRef,
+} from '@angular/core';
+import { KeyValuePipe, NgTemplateOutlet } from '@angular/common';
+import {
+  TableHeaderTemplateDirective,
+  TableRowTemplateDirective,
+} from '../../directives/template-context-guard.directive';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: 'table.component.html',
+  styleUrls: ['table.component.scss'],
+  standalone: true,
+  imports: [NgTemplateOutlet, KeyValuePipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TableComponent<TItem extends object> {
+  readonly data = input<TItem[]>([]);
+
+  readonly headers: Signal<TemplateRef<TItem> | undefined> = contentChild(
+    TableHeaderTemplateDirective,
+    { read: TemplateRef },
+  );
+
+  readonly rows: Signal<TemplateRef<TItem> | undefined> = contentChild(
+    TableRowTemplateDirective,
+    { read: TemplateRef },
+  );
+
+  constructor() {
+    effect(() => {
+      console.log(this.data());
+    });
+  }
+}
